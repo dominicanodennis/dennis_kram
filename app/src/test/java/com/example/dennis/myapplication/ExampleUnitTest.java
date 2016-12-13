d7bcfbabc5d76e2bd9fda67bb22bd9aed4719b1b
dominicanodennis <dominicanodennis@hotmail.de>
2016-12-13 01:47:46 +0100

    first commit

---
+++ app/src/test/java/com/example/dennis/myapplication/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+package com.example.dennis.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(32, 3 - 0);
+    }
+}
+
+
